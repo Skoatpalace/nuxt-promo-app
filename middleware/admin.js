b39b8d2c255cc2c3abb32f9acbb20c64e8787fda
Richard Fages <richard.fages@gmail.com>
2020-06-22 18:34:09 +0200

    add redirection pages

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,4 +1,4 @@
 export default function({ store, redirect }) {
     const isAdmin = store.getters['auth/isAdmin']
-    if (!isAdmin) redirect('/')
+    if (!isAdmin) redirect('/notAuthorized')
 }
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
 export default function({ store, redirect }) {
     const isAuth = store.getters['auth/isAuthenticated']
-    if (!isAuth) redirect('/')
+    if (!isAuth) redirect('/notAuthenticated')
 }
